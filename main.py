e62144d7bb02acd5554204765d91917c54754a28
operose123 <nfelix4@hawkmail.hccfl.edu>
2024-09-30 01:38:03 +0000

    Change ORs to ANDs

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 employeeBonus = 0.0
-productivityScore = 0
+productivityScore = 0.0
 
 employeeName = str(input("Please enter the employee's name:"))
 shiftCount = int(input("Enter employee's number of shifts completed:"))
 transactionCount = int(input("Enter employee's number of transactions made:"))
 transactionDollarValue = float(input("Enter employee's total transaction amount:"))
 
-productivityScore = ((transactionDollarValue / transactionCount) / shiftCount)
-if productivityScore <= 30:
+productivityScore = (transactionDollarValue/transactionCount)/shiftCount
+if productivityScore <= 30.0:
   employeeBonus = 50.0
 else:
-  if productivityScore >= 31 or productivityScore <= 69:
+  if productivityScore >= 31.0 and productivityScore <= 69.0:
     employeeBonus = 75.0
   else:
-    if productivityScore >= 70 or productivityScore <= 199:
+    if productivityScore >= 70.0 and productivityScore <= 199.0:
       employeeBonus = 100.0  
     else:
       employeeBonus = 200.00
